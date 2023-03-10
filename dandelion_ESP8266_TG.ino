0bd7cf8d9e38d1f9b9026a1a030de7220c6a9121
Oleksandr Mashkin <oleksandr.mashkin@gmail.com>
2023-03-10 20:18:22 +0200

    OTA added

--- dandelion_ESP8266_TG.ino
+++ dandelion_ESP8266_TG.ino
@@ -1,12 +1,16 @@
 // Board: Generic ESP8266 Module
-
+#include "danOTA.h"     //Define OTA before ESP32_cam
 #include "ESP8266_TG.h"
+
 void setup() {
   Serial.begin(115200);
   Serial.println();
   ESP8266_TG_setup();
+  OTA_init();         // danOTA.h Over The Air
+  Serial.println("Board: Generic ESP8266 Module");
 }
 
 void loop() {
   ESP8266_TG_loop();
+  ArduinoOTA.handle();
 }
